b5f4ec0eec3bedd8fa547910adb5e0238bdd6f1a
beatricemagana <beemag06@gmail.com>
2021-06-21 19:48:32 -0700

    complete class activiy

---
+++ week-19/19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,22 @@
+// Create the addNumbers function here.
+
+function addNumbers() {
+    const two = 2;
+    const three = 3;
+    const five = two + three;
+    console.log(five);
+}
+
+addNumbers();
+
+// Create the stringConcat function here.
+
+function stringConcat() {
+    const taco = 'taco';
+    const cat = 'cat';
+    const tacoCat = taco + cat + ' spelled backwards is ' + taco + cat;
+
+    alert(tacoCat);
+}
+
+stringConcat();
